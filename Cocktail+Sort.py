6432443a90b6b96ef3b2fa7c7806432ec3b97ab6
leo-chaoliu <leo@themrolab-singapore.com>
2019-10-13 18:34:39 +0800

    finished

--- Cocktail+Sort.py
+++ Cocktail+Sort.py
@@ -25,8 +25,15 @@ def bubbleSort(array, nPos, nPass):
     - sort up to and includes index [nPos]
     """
 
-    n = len(array)
+    # keep nPos+1 number to sort only
+    n = nPos + 1 
+
     for i in range(n-1, 0, -1):
+        number_of_passed = n-1-i
+
+        if number_of_passed == nPass:
+                break
+
         for j in range (1, i+1):
             if array[j-1] > array[j]:
                 swapElement(array, j, j-1) 
@@ -37,8 +44,21 @@ def cocktailSort(array, nPos, nPass):
     - stop after [nPass] 
     - sort up to and includes index [nPos]
     """
-    pass      
 
+    length = nPos + 1
+    
+    for i in range(length):
+        if i == nPass:
+            break
+
+        for j in range(length-1):
+            if array[j+1] < array[j]:
+                swapElement(array, j, j+1)
+
+        for k in range(length-1-(i+1), 0, -1):
+            if array[k] < array[k-1]:
+                swapElement(array, k, k-1)         
+ 
 def main():
 
     array = [283, 1560, 2150, 1061, 123, 2154, 222, 4]
@@ -47,28 +67,28 @@ def main():
     print(array)
 
     #change the sort algorithm to test
-    sort = bubbleSort
-    #sort = cocktailSort
+    # sort = bubbleSort
+    sort = cocktailSort
 
     #Uncomment ONE of the following sort() statement to test
     
     #this sort everything
-    #sort(array, len(array)-1, len(array))
+    # sort(array, len(array)-1, len(array))
 
     #this do one round of the algorithm
-    sort(array, len(array)-1, 1)
+    # sort(array, len(array)-1, 1)
 
     #this do two rounds of the algorithm
-    #sort(array, len(array)-1, 2)
+    sort(array, len(array)-1, 2)
 
     #this do three rounds
     #sort(array, len(array)-1, 3)
 
     #this sort the first four items 
-    #sort(array, 3, 4)
+    # sort(array, 3, 4)
 
     #this sort the first four items but 2 rounds only
-    #sort(array, 3, 2)
+    # sort(array, 3, 2)
 
     print("After :", end='')
     print(array)
