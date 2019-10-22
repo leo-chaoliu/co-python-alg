8352c7f10fa2b9c5c0bc49a1f05aa8ddd0348eae
leo-chaoliu <leo@themrolab-singapore.com>
2019-10-22 21:13:21 +0800

    stackbuks in vivo city

---
+++ week1/conclusion/helper.py
@@ -0,0 +1,9 @@
+def binary_search(array, target):
+    '''return the index of target'''
+    pass
+
+def swap(array, i, j):
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+    pass
